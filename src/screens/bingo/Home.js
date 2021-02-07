7b52d86d92fc991ce8ae46f8f3050828b52ac6ef
bekir inan <bekirinan@bekirs-MacBook-Air.local>
2021-02-07 04:00:49 +0100

    confetti effect added

--- src/screens/bingo/Home.js
+++ src/screens/bingo/Home.js
@@ -6,21 +6,19 @@ import {
   bingoPossibilityColData, 
   bingoPossibilityDiagData 
 } from '../Model/BingoModel';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-import {useStyles} from '../../styles/bingoStyles/homeStyles';
+import Confetti from 'react-confetti';
+import {useWindowSize} from 'react-use';
 var _ = require('underscore');
 
 function Home() {
   let colBingolist = [];
   const [modelData, setModelData] = useState(data);
-  const classes = useStyles();
+  const {width, height} = useWindowSize();
   const [open, setOpen] = useState(false);
   const [notes, setNotes] = useState([]);
-  const [bingoPossibilityRow, setBingoPossibilityRow] = useState(bingoPossibilityRowData);
-  const [bingoPossibilityCol, setBingoPossibilityCol] = useState(bingoPossibilityColData);
-  const [bingoPossibilityDiag, setBingoPossibilityDiag] = useState(bingoPossibilityDiagData);
+  const [bingoPossibilityRow] = useState(bingoPossibilityRowData);
+  const [bingoPossibilityCol] = useState(bingoPossibilityColData);
+  const [bingoPossibilityDiag] = useState(bingoPossibilityDiagData);
 
   useEffect(() => {
     if (notes.indexOf(12) === -1) {
@@ -92,21 +90,20 @@ function Home() {
 
   const handleOpen = () => {
     setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
+    setTimeout(() => {
+      setOpen(false);
+  }, 5000);
   };
 
   return (
     <div className="container">
         {modelData.map(data => (
           data.id === 12 ? 
-          <div key={data.id} onClick={() => choose(data.id)} className='freeSlot'>
+          <div className='freeSlot'>
             <img src="https://www.playbingoaustralia.com/wp-content/uploads/2015/02/Bingo-Night.png" alt="freeImage" width="100%"></img>
           </div> : 
           <div key={data.id} onClick={() => choose(data.id)} className={data.selected ? 'itemSelected' : 'item'}>
-            <div className="number">
+            <div className={data.selected ? 'numberSelected' : 'number'}>
               <p>{data.index}</p>
             </div>
             <div className="text">
@@ -114,24 +111,10 @@ function Home() {
             </div>
           </div>
       ))}
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <img src="https://media.giphy.com/media/1wCjfLiSZ2FWhUc2vu/giphy.gif" alt="funny GIF" width="100%" height='420px'/>
-          </div>
-        </Fade>
-      </Modal>
+     { open ? <Confetti
+      width={width}
+      height={height}
+    /> : null}
     </div>
   );
 }
